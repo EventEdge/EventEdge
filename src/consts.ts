0b8be37ed0ca0b42e06292d5b73a254fe284f2a8
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-02-09 18:48:53 +0100

    feat: add global layout

---
+++ src/consts.ts
@@ -0,0 +1,13 @@
+import type {NavigationLink} from "./models/navigation-link.model.ts";
+
+export const SITE_TITLE = "EventEdge";
+
+export const NAVIGATION_LINKS: Array<NavigationLink> = [
+    {title: 'Agenda', href: 'agenda'},
+    {title: 'Speakers', href: 'speakers'},
+    {title: 'Sponsors', href: 'sponsors'},
+    {title: 'About', href: 'about'},
+    {title: 'CoC', href: 'coc'},
+    {title: 'CFP', href: 'cfp'},
+    {title: 'Venue', href: 'venue'},
+]
