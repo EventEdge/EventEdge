e67e8c44743f15ed7f6926803857da162972752c
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-02-12 09:38:55 +0100

    fix: navigation href

--- src/consts.ts
+++ src/consts.ts
@@ -3,11 +3,11 @@ import type {INavigationLink} from "./models/navigation-link.model.ts";
 export const SITE_TITLE = "EventEdge";
 
 export const NAVIGATION_LINKS: Array<INavigationLink> = [
-    {title: 'Agenda', href: 'agenda'},
-    {title: 'Speakers', href: 'speakers'},
-    {title: 'Sponsors', href: 'sponsors'},
-    {title: 'About', href: 'about'},
-    {title: 'CoC', href: 'coc'},
-    {title: 'CFP', href: 'cfp'},
-    {title: 'Venue', href: 'venue'},
+    {title: 'Agenda', href: '/agenda'},
+    {title: 'Speakers', href: '/speakers'},
+    {title: 'Sponsors', href: '/sponsors'},
+    {title: 'About', href: '/about'},
+    {title: 'CoC', href: '/coc'},
+    {title: 'CFP', href: '/cfp'},
+    {title: 'Venue', href: '/venue'},
 ]
